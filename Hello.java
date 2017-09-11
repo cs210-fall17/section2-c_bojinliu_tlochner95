98ba352d8ad3ec4afc82db6d41e301f246c5f097
bojin liu <bojinliu@email.arizona.edu>
2017-09-11 11:18:20 -0700

    Add files via upload
    add print statement

--- Hello.java
+++ Hello.java
@@ -13,11 +13,12 @@ public class Hello {
         // Red Leader should add a print statement that says "Lock S-foils in attack position."
 	System.out.println("Lock S-foils in attack position.");
         // Gold Leader should add a print statement that says "Stabilize your rear deflectors... Watch for enemy fighters."
-
+    System.out.println("Stablize your rear deflectors... Watch for enemy fighters.")
     }
 
     public static void goldLeader() {
-
+    	
+    	System.out.println("Loosen up!");
         // Gold Leader should add a print statement that says "Loosen up!"
     
 
